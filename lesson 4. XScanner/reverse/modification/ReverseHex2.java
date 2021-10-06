f6a2a02cb54005ecedcd1f26ce49277092ac71c9
Владимир Назаров <vova24848@gmail.com>
2021-10-06 20:28:17 +0300

    complete lesson 4 and modification (0 minus)

---
+++ lesson 4. XScanner/reverse/modification/ReverseHex2.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+
+public class ReverseHex2 {
+    public static void main(String[] args) throws IOException {
+        Integer[][] ints = new Integer[0][0];
+
+        XScanner scanner = new XScanner(System.in);
+
+        int clen = 0;
+
+        while (true){
+            Integer[] line = scanner.nextIntegerLine();
+            if (line == null) {
+                break;
+            }
+
+            // copy int[+1][]
+            if (ints.length <= clen) {
+                ints = Arrays.copyOf(ints, ints.length * 2 + 1);
+            }
+
+            ints[clen] = line;
+            clen ++;
+        }
+
+        // print int[][]
+        for (int i = clen - 1; i >= 0; i--){
+            if (ints[i].length != 0){
+                for (int ii = ints[i].length - 1; ii >= 0; ii--){
+                    if (ints[i][ii] != null){
+                        System.out.print(ints[i][ii] + " ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
